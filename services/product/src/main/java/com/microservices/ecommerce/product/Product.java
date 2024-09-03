08243fd7b48f84e2bfb71aa8d0cd045e4a387675
David Lin <david11yf29@gmail.com>
2024-09-03 11:27:47 -0700

    Product-service content coding

---
+++ services/product/src/main/java/com/microservices/ecommerce/product/Product.java
@@ -0,0 +1,28 @@
+package com.microservices.ecommerce.product;
+
+import com.microservices.ecommerce.category.Category;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Integer Id;
+    private String name;
+    private String description;
+    private double availableQuantity;
+    private BigDecimal price;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
+
