f60aa8ddcaeac70cb5a1d87f764b707bdb8dff95
David Lin <david11yf29@gmail.com>
2024-09-06 13:09:36 -0700

    Finish kafka coding

--- services/order/src/main/java/com/microservices/ecommerce/orderline/OrderLineService.java
+++ services/order/src/main/java/com/microservices/ecommerce/orderline/OrderLineService.java
@@ -3,6 +3,9 @@ package com.microservices.ecommerce.orderline;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class OrderLineService {
@@ -13,5 +16,12 @@ public class OrderLineService {
     public Integer saveOrderLine(OrderLineRequest orderLineRequest) {
         var order = orderlineMapper.toOrderLine(orderLineRequest);
         return orderLineRepository.save(order).getId();
+    }
+
+    public List<OrderlineResponse> findAllByOrderId(Integer orderId) {
+        return orderLineRepository.findAllByOrderId(orderId)
+                .stream()
+                .map(orderlineMapper::toOrderLineResponse)
+                .collect(Collectors.toList());
     }
 }
